45e02b4ec2f15be354a60754128479a3a9d45710
CjEspi <espinoza.cj3@gmail.com>
2019-09-08 11:55:04 +0800

    Add html and js

---
+++ main.js
@@ -0,0 +1,9 @@
+let fn = document.getElementById('firstname').value;
+console.log(fn);
+
+function getData(){
+    let fn = document.getElementById('firstname').value;
+    let output = document.querySelector('#password');
+    output.setAttribute('value', fn);
+
+}
