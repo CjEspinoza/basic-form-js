d42e5251a49cefc919fd086f45d0523e4283547a
CjEspi <espinoza.cj3@gmail.com>
2019-09-08 11:57:20 +0800

    Revert "Add html and js"
    This reverts commit 45e02b4ec2f15be354a60754128479a3a9d45710.

--- main.js
+++
@@ -1,9 +0,0 @@
-let fn = document.getElementById('firstname').value;
-console.log(fn);
-
-function getData(){
-    let fn = document.getElementById('firstname').value;
-    let output = document.querySelector('#password');
-    output.setAttribute('value', fn);
-
-}
